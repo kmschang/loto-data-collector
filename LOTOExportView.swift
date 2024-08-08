5d8954dc1628132eda3d3105c84f3052552bf17e
Kyle Schang <kyle@schang.net>
2024-08-08 09:58:13 -0400

    updated pdf and added more fields

--- LOTOExportView.swift
+++ LOTOExportView.swift
@@ -33,7 +33,7 @@ struct LOTOExportView: View {
                     }
                     .padding(.top, 10)
                     .padding(.horizontal, 15)
-                    PDFKitView(url: url, description: item.formName)
+                    PDFKitView(url: url, description: item.formName, item: item)
                 }
             } else {
                 VStack(spacing: 10){
@@ -58,17 +58,41 @@ struct LOTOExportView: View {
     LOTOExportView(item: LOTO(formName: "", formDescription: "", procedureNumber: "", facility: "", location: "", revision: "", revisionDate: Date.now, originDate: Date.now, isolatoinPoints: "", notes: "", sourceInfo: [], machineShopSequence: "", isolateSequence: "", additionalNotes: "", completedBy: "", approvedBy: "", approvedByCompany: "", approvalDate: Date.now, status: .inProgress, favorite: .notFavorite, dateAdded: Date.now, dateEdited: Date.now))
 }
 
-func fillPDFFields(url: URL, description: String) -> PDFDocument? {
+func fillPDFFields(url: URL, description: String, item:LOTO) -> PDFDocument? {
     guard let document = PDFDocument(url: url) else { return nil }
     
     for i in 0..<document.pageCount {
         if let page = document.page(at: i) {
             for annotation in page.annotations {
                 if annotation.fieldName == "Description" {
-                    annotation.widgetStringValue = description
+                    annotation.widgetStringValue = item.formDescription
                 }
                 if annotation.fieldName == "ProcedureNumber" {
-                    annotation.widgetStringValue = description
+                    annotation.widgetStringValue = item.procedureNumber
+                }
+                if annotation.fieldName == "Facility" {
+                    annotation.widgetStringValue = item.facility
+                }
+                if annotation.fieldName == "Location" {
+                    annotation.widgetStringValue = item.location
+                }
+                if annotation.fieldName == "Revision" {
+                    annotation.widgetStringValue = item.revision
+                }
+                if annotation.fieldName == "RevisionDate" {
+                    annotation.widgetStringValue = formatDateToMMDDYY(item.revisionDate)
+                }
+                if annotation.fieldName == "OriginDate" {
+                    annotation.widgetStringValue = formatDateToMMDDYY(item.originDate)
+                }
+                if annotation.fieldName == "IsolationPoints" {
+                    annotation.widgetStringValue = item.isolatoinPoints
+                }
+                if annotation.fieldName == "Notes" {
+                    annotation.widgetStringValue = item.notes
+                }
+                if annotation.fieldName == "ID_1" {
+                    annotation.widgetStringValue = item.sourceInfo[0].source_id
                 }
             }
         }
@@ -80,10 +104,11 @@ func fillPDFFields(url: URL, description: String) -> PDFDocument? {
 struct PDFKitView: UIViewRepresentable {
     let url: URL
     let description: String
+    let item: LOTO
     
     func makeUIView(context: Context) -> PDFView {
         let pdfView = PDFView()
-        if let filledDocument = fillPDFFields(url: self.url, description: self.description) {
+        if let filledDocument = fillPDFFields(url: self.url, description: self.description, item: self.item) {
             pdfView.document = filledDocument
         } else {
             pdfView.document = PDFDocument(url: self.url)
@@ -94,4 +119,10 @@ struct PDFKitView: UIViewRepresentable {
     
     func updateUIView(_ uiView: PDFView, context: Context) {
     }
+}
+
+func formatDateToMMDDYY(_ date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "MM/dd/yy" // Define the date format
+    return dateFormatter.string(from: date)
 }
